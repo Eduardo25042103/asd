3958bccf04ce90d2b00e374efb700d33f0b74a47
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2025-04-11 04:59:54 +0000

    pasdsad

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5 import QtWidgets
-from Vista.login import Login
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    windows = Login()
-    app.exec()
